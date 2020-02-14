788596177a38a9f26f3fa8bb7a7549f1c5471ac8
Steve Davis <hmsdavis@gmail.com>
2020-02-13 21:31:56 -0500

    Works

--- index.js
+++ index.js
@@ -1,67 +1,79 @@
+/* eslint-disable no-console */
 const fs = require('fs');
 const https = require('https');
+const { performance } = require('perf_hooks');
+const axios = require('axios');
+const prompts = require('prompts');
+// eslint-disable-next-line prefer-destructuring
+const Spinner = require('cli-spinner').Spinner;
 
-// Node.js Function to save image from External URL.
 async function saveImageToDisk(url, localPath) {
-  // const fullUrl = url;
   const file = fs.createWriteStream(localPath);
   return new Promise((resolve, reject) => {
     https.get(url, (response) => {
       response.pipe(file);
-      // response.on('data', (chunk) => { rawData += chunk; });
       response.on('end', () => resolve())
-        .on('error', (e) => {
-          reject(e);
-        });
+        .on('error', (e) => reject(e));
     });
   });
 }
 
-async function getImageListFromGoogle() {
-  return new Promise((resolve, reject) => {
-    http.get('https://earthview.withgoogle.com/_api/argentina-13879.json', (res) => {
-      const { statusCode } = res;
-      const contentType = res.headers['content-type'];
-
-      let error;
-      if (statusCode !== 200) {
-        error = new Error('Request Failed.\n'
-                        + `Status Code: ${statusCode}`);
-      } else if (!/^application\/json/.test(contentType)) {
-        error = new Error('Invalid content-type.\n'
-                        + `Expected application/json but received ${contentType}`);
-      }
-      if (error) {
-        console.error(error.message);
-        // Consume response data to free up memory
-        res.resume();
-        return;
-      }
-
-      res.setEncoding('utf8');
-      let rawData = '';
-      res.on('data', (chunk) => { rawData += chunk; });
-      res.on('end', () => {
-        try {
-          const parsedData = JSON.parse(rawData);
-          console.log(parsedData);
-        } catch (e) {
-          console.error(e.message);
-        }
-      });
-    }).on('error', (e) => {
-      console.error(`Got error: ${e.message}`);
-    });
-  });
+async function getImageDataFromGoogle(urlSlug) {
+  return axios
+    .get(`https://earthview.withgoogle.com/_api/${urlSlug}.json`)
+    .then(({ data }) => ({
+      photoUrl: data.photoUrl,
+      nextSlug: data.nextSlug,
+    }));
 }
 
 (async () => {
-  try {
-    saveImageToDisk(
-      'https://www.gstatic.com/prettyearth/assets/full/13879.jpg',
-      'images/df.jpg',
-    );
-  } catch (e) {
-    console.log(e);
+  const imagesFolder = await prompts({
+    type: 'text',
+    name: 'value',
+    message: 'Where will images be saved? (/images)',
+    initial: 'images',
+  });
+  if (!fs.existsSync(imagesFolder.value)) {
+    fs.mkdirSync(imagesFolder.value);
   }
+  const saveImagesPromises = [];
+  const imagesMap = {};
+  const spinner = new Spinner({
+    text: 'Processing...',
+    stream: process.stderr,
+    onTick(msg) {
+      this.clearLine(this.stream);
+      this.stream.write(msg);
+    },
+  });
+  spinner.start();
+  let data = {};
+  let curr = 'queensland-australia-1908';
+  let i = 0;
+  const start = performance.now();
+  // eslint-disable-next-line no-constant-condition
+  while (i < 10) {
+    i += 1;
+    try {
+      /* eslint-disable no-await-in-loop */
+      data = await getImageDataFromGoogle(curr);
+    } catch (e) {
+      console.log(e);
+      break;
+    }
+    if (imagesMap[data.nextSlug]) break;
+    imagesMap[data.nextSlug] = data;
+    saveImagesPromises.push(saveImageToDisk(data.photoUrl, `${imagesFolder.value}/${curr}.jpg`));
+    curr = data.nextSlug;
+    spinner.text = `Got ${i} images so far...`;
+  }
+  spinner.stop();
+  console.clear();
+  console.log('Finishing up saving images...');
+  return Promise.all(saveImagesPromises)
+    .then(() => {
+      console.clear();
+      console.log(`Saved ${i} images in ${Math.floor(performance.now() - start)} milliseconds.`);
+    });
 })();
